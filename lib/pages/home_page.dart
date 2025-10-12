a21958d87a6ee772d5f97bda5e9a87da8a031178
EAntoni0 <ac817924@gmail.com>
2025-10-12 13:47:11 -0600

    AddBar added

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 24.0),
+          child: IconButton(
+            icon: Icon(
+              Icons.menu,
+              color: Colors.grey[800],
+              size: 36,
+            ),
+            onPressed: () {
+              // Handle menu button press
+
+            },
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: IconButton(
+              icon: Icon(
+                Icons.person,
+                color: Colors.grey[800],
+                size: 36,
+              ),
+              onPressed: () {
+                // Handle profile button press
+              },
+            ),
+          )
+        ],
+      ),
+   
+    );
+  }
+}
